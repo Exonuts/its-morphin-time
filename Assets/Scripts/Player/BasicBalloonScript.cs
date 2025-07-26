1ce3c804d518c1000786570b361c05bc22c61c4e
HugoP123 <128749707+HugoP123@users.noreply.github.com>
2025-07-26 18:25:24 +1200

    Transform Done

---
+++ Assets/Scripts/Player/BasicBalloonScript.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicBalloonScript : MonoBehaviour
+{
+    //public float moveSpeed = 5f;
+    public float moveForce;
+    public float maxSpeed;
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        // float input = Input.GetAxisRaw("Horizontal");
+        // rb.velocity = new Vector2(input * moveSpeed, rb.velocity.y);
+
+        float input = Input.GetAxisRaw("Horizontal");
+
+        if (input != 0)
+        {
+            // Apply force continuously based on input
+            rb.AddForce(Vector2.right * input * moveForce, ForceMode2D.Force);
+
+            // Clamp velocity AFTER applying force
+            float clampedX = Mathf.Clamp(rb.velocity.x, -maxSpeed, maxSpeed);
+            rb.velocity = new Vector2(clampedX, rb.velocity.y);
+        }
+    }
+
+}
--- Assets/Scripts/PlayerTransformScript.cs
+++ Assets/Scripts/PlayerTransformScript.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerTransformScript : MonoBehaviour
 {
+    public GameObject pillarPrefab;
     // Start is called before the first frame update
     void Start()
     {
