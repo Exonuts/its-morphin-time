cb0293be080971a7d343f7be86e252f2b76b4976
Ethan James <ethanjames@Ethans-Air-14.student.uod.otago.ac.nz>
2025-07-27 19:13:14 +1200

    Pushing changes

--- Assets/Scripts/NPC/BeeManager.cs
+++ Assets/Scripts/NPC/BeeManager.cs
@@ -58,7 +58,7 @@ public class BeeManager : MonoBehaviour
 
                 beesSummoned = true;
                 actualAudio.PlaySFX(beeSound);
-                for (int i = 0; i < 30; i++) {
+                for (int i = 0; i < 10; i++) {
 
                     GameObject beeToAdd = Instantiate(Resources.Load<GameObject>("Bee"),new Vector3(13.21f,1.4f,0f),Quaternion.Euler(0f,0f,0f));
                     float scale = Random.Range(0.3f,0.7f);
